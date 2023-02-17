9b0cc61c80cfecbf1ae9705260bfcca10baeb65f
Sagleft <sagleft@mfc.click>
2023-02-17 14:26:40 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install wget -y
+wget https://update.u.is/downloads/linux/utopia-latest.amd64.deb
+apt-get install gdebi-core libx11-xcb1 libgl1-mesa-glx libpulse-mainloop-glib0 libgtk-3-dev -y
+dpkg -i utopia-latest.amd64.deb
+apt-get install -f -y
+rm -rf utopia-latest.amd64.deb
+
+echo "creating new account.."
+export XDG_RUNTIME_DIR=/tmp/runtime-root
+/opt/utopia/messenger/utopia --headless --create --db=./account.db --pwd=password --configPath=./utopia.cfg
